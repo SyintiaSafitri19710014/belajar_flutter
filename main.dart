757562236c7331e08963a3779df850897ab13eca
SyintiaSafitri19710014 <85158301+SyintiaSafitri19710014@users.noreply.github.com>
2021-10-09 18:51:42 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Belajar Flutter',
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('Hello Appbar'),
+        leading: GestureDetector(
+          onTap: () {},
+          child: Icon(Icons.menu),
+        ),
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 15.0),
+            child: GestureDetector(
+              onTap: () {},
+              child: Icon(Icons.search),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 15.0),
+            child: GestureDetector(
+              onTap: () {},
+              child: Icon(Icons.more_vert),
+            ),
+          )
+        ],
+        actionsIconTheme: IconThemeData(color: Colors.black),
+      ),
+        body: Container(
+        color: Colors.blue,
+        margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        padding: EdgeInsets.all(5),
+        child: Icon(
+          Icons.home,
+          color: Colors.white,
+          size: 30.40,
+        ),
+      ),
+    );
+  }
+}
